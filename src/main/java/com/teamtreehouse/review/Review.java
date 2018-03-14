662039d0d8592b7812ed0fa756a6e2976f0162ee
Saif Tase <saif.tase@sulzer-us.com>
2018-03-14 11:10:24 -0400

    Set up API with Course and Review entities

---
+++ src/main/java/com/teamtreehouse/review/Review.java
@@ -0,0 +1,34 @@
+package com.teamtreehouse.review;
+
+import com.teamtreehouse.core.BaseEntity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Review extends BaseEntity{
+    private int rating;
+    private String description;
+
+    protected Review(){
+        super();
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
